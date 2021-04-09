b06fa051be71ea2d7ffa3fb8b17b8b91a2cbef0c
Daniil Baturin <daniil@baturin.org>
2021-04-09 18:19:48 +0700

    Update the copyright year.

--- encapcalc.js
+++ encapcalc.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Daniil Baturin and others
+// Copyright (c) 2021 Daniil Baturin and others
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
